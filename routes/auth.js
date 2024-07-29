8e346b9aabcfabf72552acc630fb2fcabb332aae
puhinetsn <puhinetsn82@gmail.com>
2024-07-29 14:55:51 +0300

    Cookie same site policy changes

--- routes/auth.js
+++ routes/auth.js
@@ -167,7 +167,8 @@ router.post('/login', async (req, res) => {
     res.cookie('jwt', token, {
         httpOnly: true,
         maxAge: 24 * 60 * 60 * 1000,
-        sameSite: 'none'
+        sameSite: 'None',
+        secure: true
     })
 
     res.send({
