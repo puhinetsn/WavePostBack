49282730021434a41429284a83d045c457b5849e
puhinetsn <puhinetsn82@gmail.com>
2024-07-29 14:29:08 +0300

    Deployment changes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:4200'
+    origin: 'https://puhinetsn.github.io/'
 }))
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs, { explorer: true }));
